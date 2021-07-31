184c5b381c065eed55d16b578e8155ad9109e857
AhmadAmr <ahmedamr18841@gmail.com>
2021-07-31 09:17:40 +0200

    DropDown

--- Blog-App-policy-permission/Pages/Index.cshtml.cs
+++ Blog-App-policy-permission/Pages/Index.cshtml.cs
@@ -2,17 +2,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blog_App_policy_permission.Data;
+using Blog_App_policy_permission.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog_App_policy_permission.Pages
 {
     [AllowAnonymous]
-    public class HelloModel : PageModel
+    public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _contex;
+
+        public IndexModel(ApplicationDbContext context)
         {
+            _contex = context;
+        }
+        
+        public IList<Models.Blog> Blog { get; private set; }
+        public async Task OnGetAsync()
+        {
+            var blog = _contex.Blogs.Where(x => x.Published && x.Status == BlogStatus.Approved);
+            Blog = await blog.ToListAsync();
         }
     }
 }
